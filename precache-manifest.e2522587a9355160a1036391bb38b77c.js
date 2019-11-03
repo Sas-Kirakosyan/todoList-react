9cbbf4be2116261482a797b3a81f609c2703f481
sas <sasun-kirakosyan@mail.ru>
2019-11-03 21:00:03 +0400

    Updates

---
+++ precache-manifest.e2522587a9355160a1036391bb38b77c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04015fd5a24d9fcd1ae08811c7358979",
+    "url": "/todoList-react/index.html"
+  },
+  {
+    "revision": "437144f2a46498e42617",
+    "url": "/todoList-react/static/css/main.383a99c3.chunk.css"
+  },
+  {
+    "revision": "dc4608e757104ac21031",
+    "url": "/todoList-react/static/js/2.7078ed54.chunk.js"
+  },
+  {
+    "revision": "437144f2a46498e42617",
+    "url": "/todoList-react/static/js/main.595958f8.chunk.js"
+  },
+  {
+    "revision": "7f9687a007236d7814dc",
+    "url": "/todoList-react/static/js/runtime-main.f89f1002.js"
+  }
+]);
