ef5b5ae0c7d86c8b8f7a9701fa9a4384590c6bdb
Sas-Kirakosyan <47293247+Sas-Kirakosyan@users.noreply.github.com>
2019-11-16 16:16:49 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,56 @@
 import React from 'react';
 import './App.css';
  import List from './components/List'
- import CloseButton from './components/CloseButton'
+
+
+
 
 class mainTodo extends React.Component{
 constructor(props){
   super(props)
   this.state=({
-    terms: '',
-    items:[],
-    close: ''
-  })
-}
-
-handleInput =(e)=>{
-this.setState({
-  terms: e.target.value,
-  
+ 
+    text: '',
+  listItems:[],
+    check: false,
     })
 }
-submitHendle =(e)=>{
-  e.preventDefault();
-  this.setState({
-     items: [...this.state.items, this.state.terms]
-   
+handleInput =(e)=>{
+this.setState({
+  text: e.target.value,
   })
 }
+submitHendle =(e)=>{
+  console.log(e)
+  let text = this.state.text;
+  let id = this.state.id;
+  let listItem = this.state.listItems
+ e.preventDefault();
+  this.setState({
+   listItems: [...listItem, text]
+   
+   })
+  
+}
+removeItem=(e)=>{
+ this.setState({
+   listItem: this.state.listItems.splice(e, 1)
+ })
+ 
+}
+
 render(){
   return(
   <div className='container'>
     <h1 className='h1'>My todo list</h1>
-    <form className="App" onSubmit={this.submitHendle} >
-     <input type="text" onChange={this.handleInput} value={this.state.terms} />
-     <button>Add</button>
+   <form className="App">
+       <input type="text" onChange={this.handleInput} value={this.state.text} />
+       <button onClick={this.submitHendle}>Add</button>
     </form>
-
-    <List items={this.state.items} ><CloseButton /></List>
+  {/* pastoreyn onsubmit@ formi mej poxarinum a buttoni meji onclickin */}
+    <List removeItem={this.removeItem} items={this.state.listItems} >
+  </List>
   </div>)
   }
 }
 export default mainTodo;
-
