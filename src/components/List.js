27ea1816f9c024339e2ea6a4f3103835985df44d
Sas-Kirakosyan <47293247+Sas-Kirakosyan@users.noreply.github.com>
2019-11-16 16:15:34 +0400

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -2,18 +2,24 @@ import React from 'react';
 import '../App.css';
 import CloseButton from  './CloseButton'
 
-const List = (props)=>{
-    return(
-       <ul className='ul'>
-           {
-               props.items.map((item, index) => 
-               <li className='li' key={index} >{item} <CloseButton /></li>
-               
-               )
-           }
-           
-       </ul>
-    )
+class List extends React.Component {
+  
+ render(){
+        return(
+            <ul className='ul'>
+                {
+                    this.props.items.map((item, index) => 
+                    <li className='li' key={index} >{item} 
+                    <CloseButton key={index} handleClick={(e)=>this.props.removeItem(index)} />
+                </li>
+                    )
+                }
+                
+            </ul>
+         )
+    }
+ 
 }
 export default List
+
 
